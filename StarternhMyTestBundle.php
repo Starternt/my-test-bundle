36007cf2f0a2fa662f4624a39867c274ed3c7cf4
Лактионов Константин <klaktionov@kr.digital>
2023-04-24 19:05:57 +0300

    fix autoload

--- StarternhMyTestBundle.php
+++ StarternhMyTestBundle.php
@@ -2,8 +2,13 @@
 
 namespace Starternh\MyTestBundle;
 
+use Starternh\MyTestBundle\DependencyInjection\StarternhMyTestExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class StarternhMyTestBundle extends Bundle
 {
+    public function getContainerExtension(): StarternhMyTestExtension
+    {
+        return new StarternhMyTestExtension();
+    }
 }
