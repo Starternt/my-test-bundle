f4b9cd7baf798afc6f9e37f9d75ec5a82ae9d69c
Лактионов Константин <klaktionov@kr.digital>
2023-04-26 17:47:53 +0300

    Revert "test"
    This reverts commit 481d19ec042b856a18c74a7ec44bb33a516a3c1f.

--- StarternhMyTestBundle.php
+++ StarternhMyTestBundle.php
@@ -3,17 +3,10 @@
 namespace Starternh\MyTestBundle;
 
 use Starternh\MyTestBundle\DependencyInjection\StarternhMyTestExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class StarternhMyTestBundle extends Bundle
 {
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-        $container->addCompilerPass(new DependencyInjection\Compiler\CorsConfigurationProviderPass());
-    }
-
     public function getContainerExtension(): StarternhMyTestExtension
     {
         return new StarternhMyTestExtension();
