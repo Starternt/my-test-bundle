62510496b36aea7d1e1ef080bd6bade127a044cf
Лактионов Константин <klaktionov@kr.digital>
2023-04-26 20:53:53 +0300

    test

--- DependencyInjection/StarternhMyTestExtension.php
+++ DependencyInjection/StarternhMyTestExtension.php
@@ -60,7 +60,7 @@ class StarternhMyTestExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
