8182f37e9e8bc5734719a13858ee521fe59b5bc5
Лактионов Константин <klaktionov@kr.digital>
2023-04-26 21:26:57 +0300

    Revert "test"
    This reverts commit 4a8822fd8dd1774662e3fe42ddea59fa9fd2db9b.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -7,64 +7,18 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    // /**
-    //  * {@inheritdoc}
-    //  */
-    // public function getConfigTreeBuilder()
-    // {
-    //     $treeBuilder = new TreeBuilder('my_test');
-    //     $treeBuilder->getRootNode()
-    //         ->children()
-    //         ->booleanNode('isDisplay')->defaultTrue()->end()
-    //         ->booleanNode('someBoolean')->defaultTrue()->end()
-    //         ->end()
-    //     ;
-    //     return $treeBuilder;
-    // }
-
     /**
-     * Generates the configuration tree.
-     *
-     * @return TreeBuilder
+     * {@inheritdoc}
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('gregwar_captcha');
-        $rootNode = $treeBuilder->getRootNode();
-
-        $rootNode
-            ->addDefaultsIfNotSet()
+        $treeBuilder = new TreeBuilder('my_test');
+        $treeBuilder->getRootNode()
             ->children()
-            ->scalarNode('length')->defaultValue(5)->end()
-            ->scalarNode('width')->defaultValue(130)->end()
-            ->scalarNode('height')->defaultValue(50)->end()
-            ->scalarNode('font')->defaultValue(__DIR__.'/../Generator/Font/captcha.ttf')->end()
-            ->scalarNode('keep_value')->defaultValue(false)->end()
-            ->scalarNode('charset')->defaultValue('abcdefhjkmnprstuvwxyz23456789')->end()
-            ->scalarNode('as_file')->defaultValue(false)->end()
-            ->scalarNode('as_url')->defaultValue(false)->end()
-            ->scalarNode('reload')->defaultValue(false)->end()
-            ->scalarNode('image_folder')->defaultValue('captcha')->end()
-            ->scalarNode('web_path')->defaultValue('%kernel.root_dir%/../web')->end()
-            ->scalarNode('gc_freq')->defaultValue(100)->end()
-            ->scalarNode('expiration')->defaultValue(60)->end()
-            ->scalarNode('quality')->defaultValue(50)->end()
-            ->scalarNode('invalid_message')->defaultValue('Bad code value')->end()
-            ->scalarNode('bypass_code')->defaultValue(null)->end()
-            ->scalarNode('whitelist_key')->defaultValue('captcha_whitelist_key')->end()
-            ->scalarNode('humanity')->defaultValue(0)->end()
-            ->scalarNode('distortion')->defaultValue(true)->end()
-            ->scalarNode('max_front_lines')->defaultValue(null)->end()
-            ->scalarNode('max_behind_lines')->defaultValue(null)->end()
-            ->scalarNode('interpolation')->defaultValue(true)->end()
-            ->arrayNode('text_color')->prototype('scalar')->end()->end()
-            ->arrayNode('background_color')->prototype('scalar')->end()->end()
-            ->arrayNode('background_images')->prototype('scalar')->end()->end()
-            ->scalarNode('disabled')->defaultValue(false)->end()
-            ->scalarNode('ignore_all_effects')->defaultValue(false)->end()
+            ->booleanNode('isDisplay')->defaultTrue()->end()
+            ->booleanNode('someBoolean')->defaultTrue()->end()
             ->end()
         ;
-
         return $treeBuilder;
     }
 }
--- DependencyInjection/StarternhMyTestExtension.php
+++ DependencyInjection/StarternhMyTestExtension.php
@@ -5,7 +5,6 @@ namespace Starternh\MyTestBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -16,63 +15,40 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class StarternhMyTestExtension extends Extension
 {
-    // /**
-    //  * {@inheritdoc}
-    //  */
-    // public function load(array $configs, ContainerBuilder $container)
-    // {
-    //     // Has what is in config/packages/owner_test.yaml
-    //     // dump($configs);
-    //     //
-    //     // $configuration = new Configuration();
-    //     //
-    //     // $config = $this->processConfiguration($configuration, $configs);
-    //     // dump($config);
-    //
-    //     // At this point you would use $config to define your parameters or services
-    //
-    //     $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-    //     $loader->load('services.yaml');
-    //
-    //     $configuration = new Configuration();
-    //     $config = $this->processConfiguration($configuration, $configs);
-    //     // $processor = new Processor();
-    //     // $configuration = $this->getConfiguration($configs, $container);
-    //
-    //     foreach ($config as $key => $value) {
-    //         $container->setParameter('my_test.' . $key, $value);
-    //     }
-    //     // $container->setParameter('my_test.config', $config);
-    // }
-
-    // /**
-    //  * {@inheritdoc}
-    //  */
-    // public function getConfiguration(array $config, ContainerBuilder $container)
-    // {
-    //     return new Configuration();
-    // }
-
     /**
-     * @param array $configs
-     * @param ContainerBuilder $container
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        // Has what is in config/packages/owner_test.yaml
+        // dump($configs);
+        //
+        // $configuration = new Configuration();
+        //
+        // $config = $this->processConfiguration($configuration, $configs);
+        // dump($config);
+
+        // At this point you would use $config to define your parameters or services
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+        // $processor = new Processor();
+        // $configuration = $this->getConfiguration($configs, $container);
 
-        $container->setParameter('gregwar_captcha.config', $config);
-        $container->setParameter('gregwar_captcha.config.image_folder', $config['image_folder']);
-        $container->setParameter('gregwar_captcha.config.web_path', $config['web_path']);
-        $container->setParameter('gregwar_captcha.config.gc_freq', $config['gc_freq']);
-        $container->setParameter('gregwar_captcha.config.expiration', $config['expiration']);
-        $container->setParameter('gregwar_captcha.config.whitelist_key', $config['whitelist_key']);
+        foreach ($config as $key => $value) {
+            $container->setParameter('my_test.' . $key, $value);
+        }
+        // $container->setParameter('my_test.config', $config);
+    }
 
-        $resources = $container->getParameter('twig.form.resources');
-        $container->setParameter('twig.form.resources', array_merge(array('@GregwarCaptcha/captcha.html.twig'), $resources));
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfiguration(array $config, ContainerBuilder $container)
+    {
+        return new Configuration();
     }
 }
