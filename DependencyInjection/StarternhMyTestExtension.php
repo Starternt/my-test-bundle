870d809ff6a5da2fda929475a5081ab8a7374557
Лактионов Константин <klaktionov@kr.digital>
2023-04-26 21:26:57 +0300

    Revert "test"
    This reverts commit 9f97b50eab7b4782cb693ab6bd87f8c7238f2735.

--- DependencyInjection/StarternhMyTestExtension.php
+++ DependencyInjection/StarternhMyTestExtension.php
@@ -71,6 +71,8 @@ class StarternhMyTestExtension extends Extension
         $container->setParameter('gregwar_captcha.config.gc_freq', $config['gc_freq']);
         $container->setParameter('gregwar_captcha.config.expiration', $config['expiration']);
         $container->setParameter('gregwar_captcha.config.whitelist_key', $config['whitelist_key']);
-        $container->compile();
+
+        $resources = $container->getParameter('twig.form.resources');
+        $container->setParameter('twig.form.resources', array_merge(array('@GregwarCaptcha/captcha.html.twig'), $resources));
     }
 }
