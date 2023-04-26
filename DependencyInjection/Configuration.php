0536623dba4b0c44e60ebc809f8c32fbb0844921
Лактионов Константин <klaktionov@kr.digital>
2023-04-26 17:47:53 +0300

    Revert "test"
    This reverts commit 22d924a30a585c34d93a695a8def8fa3dc1e459e.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,168 +2,30 @@
 
 namespace Starternh\MyTestBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\Definition\Builder\BooleanNodeDefinition;
-use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
-    public function getConfigTreeBuilder(): TreeBuilder
+    public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('my_test');
+        $treeBuilder = new TreeBuilder('starternh_my_test');
 
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC for symfony/config < 4.2
-            $rootNode = $treeBuilder->root('my_test');
-        }
-
-        $rootNode
+        $treeBuilder->getRootNode()
             ->children()
-            ->arrayNode('defaults')
-            ->addDefaultsIfNotSet()
-            ->append($this->getAllowCredentials())
-            ->append($this->getAllowOrigin())
-            ->append($this->getAllowHeaders())
-            ->append($this->getAllowMethods())
-            ->append($this->getExposeHeaders())
-            ->append($this->getMaxAge())
-            ->append($this->getHosts())
-            ->append($this->getOriginRegex())
-            ->append($this->getForcedAllowOriginValue())
-            ->end()
-
-            ->arrayNode('paths')
-            ->useAttributeAsKey('path')
-            ->normalizeKeys(false)
-            ->prototype('array')
-            ->append($this->getAllowCredentials())
-            ->append($this->getAllowOrigin())
-            ->append($this->getAllowHeaders())
-            ->append($this->getAllowMethods())
-            ->append($this->getExposeHeaders())
-            ->append($this->getMaxAge())
-            ->append($this->getHosts())
-            ->append($this->getOriginRegex())
-            ->append($this->getForcedAllowOriginValue())
-            ->end()
+                ->arrayNode('my_test')
+                    ->children()
+                        ->integerNode('base_url')->end()
+                        ->scalarNode('token')->end()
+                    ->end()
+                ->end()
             ->end()
         ;
 
         return $treeBuilder;
-    }
-
-    private function getAllowCredentials(): BooleanNodeDefinition
-    {
-        $node = new BooleanNodeDefinition('allow_credentials');
-        $node->defaultFalse();
-
-        return $node;
-    }
-
-    private function getAllowOrigin(): ArrayNodeDefinition
-    {
-        $node = new ArrayNodeDefinition('allow_origin');
-
-        $node
-            ->beforeNormalization()
-            ->always(function ($v) {
-                if ($v === '*') {
-                    return ['*'];
-                }
-
-                return $v;
-            })
-            ->end()
-            ->prototype('scalar')->end()
-        ;
-
-        return $node;
-    }
-
-    private function getAllowHeaders(): ArrayNodeDefinition
-    {
-        $node = new ArrayNodeDefinition('allow_headers');
-
-        $node
-            ->beforeNormalization()
-            ->always(function ($v) {
-                if ($v === '*') {
-                    return ['*'];
-                }
-
-                return $v;
-            })
-            ->end()
-            ->prototype('scalar')->end();
-
-        return $node;
-    }
-
-    private function getAllowMethods(): ArrayNodeDefinition
-    {
-        $node = new ArrayNodeDefinition('allow_methods');
-
-        $node->prototype('scalar')->end();
-
-        return $node;
-    }
-
-    private function getExposeHeaders(): ArrayNodeDefinition
-    {
-        $node = new ArrayNodeDefinition('expose_headers');
-
-        $node->prototype('scalar')->end();
-
-        return $node;
-    }
-
-    private function getMaxAge(): ScalarNodeDefinition
-    {
-        $node = new ScalarNodeDefinition('max_age');
-
-        $node
-            ->defaultValue(0)
-            ->validate()
-            ->ifTrue(function ($v) {
-                return !is_numeric($v);
-            })
-            ->thenInvalid('max_age must be an integer (seconds)')
-            ->end()
-        ;
-
-        return $node;
-    }
-
-    private function getHosts(): ArrayNodeDefinition
-    {
-        $node = new ArrayNodeDefinition('hosts');
-
-        $node->prototype('scalar')->end();
-
-        return $node;
-    }
-
-    private function getOriginRegex(): BooleanNodeDefinition
-    {
-        $node = new BooleanNodeDefinition('origin_regex');
-        $node->defaultFalse();
-
-        return $node;
-    }
-
-    private function getForcedAllowOriginValue(): ScalarNodeDefinition
-    {
-        $node = new ScalarNodeDefinition('forced_allow_origin_value');
-        $node->defaultNull();
-
-        return $node;
     }
 }
 
